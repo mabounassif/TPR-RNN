294e6f32a291f114a05808cddcb57793fe42f85d
Mahmoud Nassif <mabounassif@Mahmouds-MacBook-Air.local>
2019-04-23 17:54:11 -0400

    Temp milestone for sharing

--- tpr_rnn_graph.py
+++ tpr_rnn_graph.py
@@ -16,6 +16,7 @@ import os
 from preprocessor.reader import parse
 from lib import *
 
+import t3f
 
 ###### Hyper Parameters ------------------
 c = types.SimpleNamespace()
@@ -105,7 +106,7 @@ with tf.variable_scope("hyper_params", reuse=None, dtype=tf.float32):
   _beta2 = tf.get_variable("beta2", shape=[], trainable=False)
 
   # op to set hyper parameters
-  hyper_param_init = [ 
+  hyper_param_init = [
     _learning_rate.assign(c.learning_rate),
     _beta1.assign(c.beta1),
     _beta2.assign(c.beta2)
@@ -117,37 +118,39 @@ with tf.variable_scope("inputs"):
   story_length = tf.placeholder(dtype=tf.int32, shape=[None], name='story_length') # [batch_size]
   query = tf.placeholder(dtype=tf.int32, shape=[None, None], name='query')  # [batch_size, words]
   answer = tf.placeholder(dtype=tf.int32, shape=[None], name='answer')  # [batch_size]
+  tt_tpr_init = tf.placeholder(dtype=tf.int32, shape=[None, c.entity_size, c.role_size, c.entity_size], name='tt_tpr_init')
 
-  _batch_size = tf.shape(story)[0]
-  sentence_length = tf.shape(story)[2]
+  _batch_size = 32
+  sentence_length = 12
 
 with tf.variable_scope("variables"): # Note, the MLP weights are created in the MLP function.
-  # initialize the embeddings  
-  word_embedding = tf.get_variable(name="word_embedding", 
-                                   shape=[c.vocab_size, c.symbol_size], 
+  # initialize the embeddings
+  word_embedding = tf.get_variable(name="word_embedding",
+                                   shape=[c.vocab_size, c.symbol_size],
                                    initializer=uniform_init(c.init_limit))
-  position = tf.get_variable(name="story_position_embedding", 
+  position = tf.get_variable(name="story_position_embedding",
                                    shape=[max_sentence_length, c.symbol_size], # [words, symbol_size]
                                    initializer=ones_init()) / max_sentence_length
-  Z = tf.get_variable(name="output_embedding", 
-                      shape=[c.entity_size, c.vocab_size], 
+  Z = tf.get_variable(name="output_embedding",
+                      shape=[c.entity_size, c.vocab_size],
                       initializer=uniform_glorot_init(c.entity_size, c.vocab_size)) # output projection Z, final transformation
 
   # initial state of the TPR
-  TPR_init = tf.zeros((_batch_size, c.entity_size, c.role_size, c.entity_size))
+  tt_tpr_init = [t3f.to_tt_tensor(tf.zeros((c.entity_size, c.role_size, c.entity_size))).tt_cores for i in range(_batch_size)]
+  print('prezipped shape:', len(tt_tpr_init))
 
   # we use a cell and dynamic_rnn instead of a tf.while_loop due to its dynamic squence_length capability
-  loopCell = LoopCell((_batch_size, c.entity_size, c.role_size, c.entity_size))
+  loopCell = LoopCell(tt_tpr_init)
 
 with tf.variable_scope("model"):
   with tf.variable_scope("update_module"):
     # we had problems with embedding_lookup and the optimizer implementation.
     # [batch_size, sentences, words, embedding_size]
-    #sentence_emb = tf.nn.embedding_lookup(params=word_embedding, ids=story) 
-    sentence_emb = tf.einsum('bswv,ve->bswe', tf.one_hot(story, depth=c.vocab_size), word_embedding)  
+    #sentence_emb = tf.nn.embedding_lookup(params=word_embedding, ids=story)
+    sentence_emb = tf.einsum('bswv,ve->bswe', tf.one_hot(story, depth=c.vocab_size), word_embedding)
     # [batch_size, words, embedding_size]
-    #query_emb = tf.nn.embedding_lookup(params=word_embedding, ids=query)  
-    query_emb = tf.einsum('bwv,ve->bwe', tf.one_hot(query, depth=c.vocab_size), word_embedding) 
+    #query_emb = tf.nn.embedding_lookup(params=word_embedding, ids=query)
+    query_emb = tf.einsum('bwv,ve->bwe', tf.one_hot(query, depth=c.vocab_size), word_embedding)
 
     # summing over the words of a sentence into sentence representations
     # [batch_size, sentences, embedding_size]
@@ -156,9 +159,9 @@ with tf.variable_scope("model"):
     query_sum = tf.einsum('bwe,we->be', query_emb, position) # eq.5 for the question sentence
 
     # Five MLPs that extract the entity and relation representations
-    e1, e2 = MLP(sentence_sum, n_networks=2, equation='bse,er->bsr', input_size=c.symbol_size, 
+    e1, e2 = MLP(sentence_sum, n_networks=2, equation='bse,er->bsr', input_size=c.symbol_size,
                    hidden_size=c.hidden_size, output_size=c.entity_size, scope="story_entity")
-    r1, r2, r3 = MLP(sentence_sum, n_networks=3, equation='bse,er->bsr', input_size=c.symbol_size, 
+    r1, r2, r3 = MLP(sentence_sum, n_networks=3, equation='bse,er->bsr', input_size=c.symbol_size,
                    hidden_size=c.hidden_size, output_size=c.role_size, scope="story_roles")
 
     # compute part of the tensor update outside the loop for efficency
@@ -166,9 +169,12 @@ with tf.variable_scope("model"):
     partial_add_W = tf.einsum('bsr,bsf->bsrf', r1, e2) # part of eq.10
     partial_add_B = tf.einsum('bsr,bsf->bsrf', r3, e1) # part of eq.14
 
-    # perform loop operation using dynamic_rnn (so we can exploit dynamic sequence lengths)  
-    def body(inputs, TPR):
-      e1, r1, partial_add_W, e2, r2, partial_add_B, r3 = inputs 
+    # perform loop operation using dynamic_rnn (so we can exploit dynamic sequence lengths)
+    def body(inputs, tt_cores_batch):
+      TPR = tf.stack([t3f.full(t3f.TensorTrain(tt_cores)) for tt_cores in tt_cores_batch])
+
+      e1, r1, partial_add_W, e2, r2, partial_add_B, r3 = inputs
+      #  TPR = t3f.Tra
       # e1 and e2 are [batch_size, entity_size]
       # r1 and r2 are [batch_size, role_size]
       # TPR is [batch_size, entity_size, role_size, entity_size]
@@ -183,7 +189,7 @@ with tf.variable_scope("model"):
 
       b_hat = (tf.einsum('be,br,berf->bf', e2, r3, TPR)) # eq. 13
       partial_remove_B = tf.einsum('br,bf->brf', r3, b_hat) # part of eq.14
-      
+
       # tensor product obeys a distributive law with the direct sum operation
       # this allows for a more efficient implementation
       # we first add the ops before we go from order 2 to order 3
@@ -193,22 +199,29 @@ with tf.variable_scope("model"):
       delta_F = tf.einsum('be,brf->berf', e1, write_op + move_op) \
                 + tf.einsum('be,brf->berf', e2, backlink_op) # eq. 6
 
-      # direct sum of the old state with the new ones. Removes old associations and adds new ones. 
+      # direct sum of the old state with the new ones. Removes old associations and adds new ones.
       TPR += delta_F # eq. 4
 
-      return [delta_F], TPR
+      u_tpr = tf.unstack(TPR)
+      u_delta = tf.unstack(delta_F)
 
-    # we set the body of our empty loop cell and make use of 
+      return [[t3f.to_tt_tensor(d).tt_cores for d in u_delta]], [t3f.to_tt_tensor(t).tt_cores for t in u_tpr]
+
+    # we set the body of our empty loop cell and make use of
     # the dynamic sequence_length capability of dynamic_rnn
     loopCell.call = body
     inputs = (e1, r1, partial_add_W, e2, r2, partial_add_B, r3) # all input tensors are already batch major
-    _, TPR = tf.nn.dynamic_rnn(loopCell, inputs, initial_state=TPR_init, sequence_length=story_length)
+
+    _, tt_tpr_cores = tf.nn.dynamic_rnn(loopCell, inputs, initial_state=tt_tpr_init, sequence_length=story_length)
+
+    TPR = tf.stack([t3f.full(t3f.TensorTrain(tt_cores)) for tt_cores in tt_tpr_cores])
+    print('unziped shape:', TPR.shape)
 
   with tf.variable_scope("inference_module"):
     # for the question we use the same sentence encoding but different MLPs (these are used in the inference module)
-    q_e1, q_e2 = MLP(query_sum, n_networks=2, equation='be,er->br', input_size=c.symbol_size, 
+    q_e1, q_e2 = MLP(query_sum, n_networks=2, equation='be,er->br', input_size=c.symbol_size,
                      hidden_size=c.hidden_size, output_size=c.entity_size, scope="query_entity")
-    q_r1, q_r2, q_r3 = MLP(query_sum, n_networks=3, equation='be,er->br', input_size=c.symbol_size, 
+    q_r1, q_r2, q_r3 = MLP(query_sum, n_networks=3, equation='be,er->br', input_size=c.symbol_size,
                            hidden_size=c.hidden_size, output_size=c.role_size, scope="query_roles")
 
     ## compute question answer ((b)atch, (e)ntity, (r)ole, (f)iller, (q)ueries)
@@ -249,11 +262,13 @@ with tf.variable_scope("optimizer"):
   gradients = tf.gradients(cost, trainable_vars)
   global_norm = tf.global_norm(gradients) # compute global norm
   clipped_global_norm = tf.where(tf.is_nan(global_norm), c.max_gradient_norm, global_norm) # clip NaN gradients to max norm
-  clipped_gradients, gradient_norm = tf.clip_by_global_norm(t_list=gradients, 
+  clipped_gradients, gradient_norm = tf.clip_by_global_norm(t_list=gradients,
                                                             clip_norm=c.max_gradient_norm,
                                                             use_norm=clipped_global_norm) # uses this norm instead of computing it
   train_op = optimizer.apply_gradients(zip(clipped_gradients, trainable_vars))
 
+builder = tf.profiler.ProfileOptionBuilder
+opts = builder(builder.time_and_memory()).order_by('micros').build()
 
 ###### Session and Other---------
 with tf.variable_scope("session_and_other"):
@@ -287,6 +302,12 @@ def get_feed_dic(batch):
     query: batch[2],
     answer: batch[3],
   }
+
+  print(batch[0].shape[0])
+  print(batch[1].shape)
+  print(batch[2].shape)
+  print(batch[3].shape)
+
   return feed_dic
 
 def train(steps=1000000, bs=128, terminal_log_every=50, validate_every=200):
@@ -296,89 +317,102 @@ def train(steps=1000000, bs=128, terminal_log_every=50, validate_every=200):
   # ---
   # reinitializing the iterator resets it to the first batch
   sess.run(train_iterator.initializer, {batch_size: bs})
-  
+
   _start_time = time.time()
   _prev_time = _start_time
   _prev_step = 0
 
   acc_sum, cost_sum, valid_acc, valid_cost = 0, 0, 0, 999
 
-  for i in range(steps):
-    # reduce learning rate for warm up period
-    if i < c.warm_up_steps and c.do_warm_up:
-      sess.run(_learning_rate.assign(c.learning_rate * c.warm_up_factor))
+  with tf.contrib.tfprof.ProfileContext('./abounasm/profiler', trace_steps=[], dump_steps=[]) as pctx:
+      for i in range(steps):
+        # reduce learning rate for warm up period
+        if i < c.warm_up_steps and c.do_warm_up:
+          sess.run(_learning_rate.assign(c.learning_rate * c.warm_up_factor))
 
-    if i > c.warm_up_steps and c.do_warm_up:
-      sess.run(_learning_rate.assign(c.learning_rate))
+        if i > c.warm_up_steps and c.do_warm_up:
+          sess.run(_learning_rate.assign(c.learning_rate))
 
-    # decay learning once
-    if valid_cost < c.decay_thresh and c.do_decay and not _decay_done:
-      saver.save(sess, os.path.join(c.log_folder, "preReduction.ckpt"))
-      sess.run(_learning_rate.assign(c.learning_rate * c.decay_factor))
-      _decay_done = True # prevent further decays
-    
-    # get a batch and run a step
-    batch = sess.run(train_batch) # batch size is defined by the iterator initialization
-    feed_dic = get_feed_dic(batch)
-    query_dic = {
-      _learning_rate.name: _learning_rate,
-      train_op.name: train_op,
-      cost.name: cost,
-      accuracy.name: accuracy,
-      gradient_norm.name: gradient_norm
-    }
-    result = sess.run(query_dic, feed_dic)
-    cost_sum += result[cost.name]
-    acc_sum += result[accuracy.name]
+        # decay learning once
+        if valid_cost < c.decay_thresh and c.do_decay and not _decay_done:
+          saver.save(sess, os.path.join(c.log_folder, "preReduction.ckpt"))
+          sess.run(_learning_rate.assign(c.learning_rate * c.decay_factor))
+          _decay_done = True # prevent further decays
 
-    # if we have NaN values during the warm_up phase we restart training
-    if np.isnan(result[cost.name]) and i < c.warm_up_steps:
-      log("NaN cost during warmup, reinitializing.")
-      _total_steps = 0
-      i = 0
-      sess.run(tf.global_variables_initializer())
-      sess.run(tf.local_variables_initializer())
-      sess.run(hyper_param_init)
-      continue
+        options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+        run_meta = tf.RunMetadata()
+        # get a batch and run a step
+        batch = sess.run(train_batch) # batch size is defined by the iterator initialization
+        feed_dic = get_feed_dic(batch)
 
-    # print and log the current training performance
-    if (_total_steps % terminal_log_every == 0) or np.isnan(result[cost.name]) or _total_steps < c.warm_up_steps:
-      # track speed
-      epochs_seen = (_total_steps * bs) / train_epoch_size
-      time_done = (time.time() - _prev_time) / 60.0 # in minutes
-      step_speed = (i - _prev_step) / time_done
-      stories_speed = step_speed * bs
-      _prev_time = time.time()
-      _prev_step = i
+        query_dic = {
+          _learning_rate.name: _learning_rate,
+          train_op.name: train_op,
+          cost.name: cost,
+          accuracy.name: accuracy,
+          gradient_norm.name: gradient_norm
+        }
 
-      log("{:4}: cost={:6.4f}, accuracy={:7.4f}, norm={:06.3f}, lr={:.4f} (epochs={:.1f}, steps/min={:2.0f}, stories/min={:2.0f})".format(
-            _total_steps, result[cost.name], result[accuracy.name], result[gradient_norm.name], result[_learning_rate.name],
-            epochs_seen, step_speed, stories_speed))
-      
-      writer.add_summary(make_summary(tag="train/cost", value=result[cost.name]), _total_steps)
-      writer.add_summary(make_summary(tag="train/accuracy", value=result[accuracy.name]), _total_steps)
+        pctx.trace_next_step()
+        pctx.dump_next_step()
 
-    # print and log the current validation and test performance
-    if _total_steps % validate_every == 0 and _total_steps != 0:
-      log("")
-      log("task: ", c.task_id)
-      valid_cost, valid_acc = eval(prefix="valid", batch_source=valid_batch, steps=valid_steps, bs=valid_batch_size)
-      test_cost, test_acc = eval(prefix="test", batch_source=test_batch, steps=test_steps, bs=test_batch_size)
+        result = sess.run(query_dic, feed_dic, options=options, run_metadata=run_meta)
 
-      writer.add_summary(make_summary(tag="valid/cost", value=valid_cost), _total_steps)
-      writer.add_summary(make_summary(tag="valid/accuracy", value=valid_acc), _total_steps)
-      writer.add_summary(make_summary(tag="test/cost", value=test_cost), _total_steps)
-      writer.add_summary(make_summary(tag="test/accuracy", value=test_acc), _total_steps)
 
-      log("Total time passed: {:.1f} min".format((time.time() - _start_time) / 60.0))
-      log("")
+        cost_sum += result[cost.name]
+        acc_sum += result[accuracy.name]
 
-      if valid_acc >= _best_valid_acc:
-        saver.save(sess, os.path.join(c.log_folder, "model.ckpt"))
+        # if we have NaN values during the warm_up phase we restart training
+        if np.isnan(result[cost.name]) and i < c.warm_up_steps:
+          log("NaN cost during warmup, reinitializing.")
+          _total_steps = 0
+          i = 0
+          sess.run(tf.global_variables_initializer())
+          sess.run(tf.local_variables_initializer())
+          sess.run(hyper_param_init)
+          continue
 
-    _total_steps += 1  
+        # print and log the current training performance
+        if (_total_steps % terminal_log_every == 0) or np.isnan(result[cost.name]) or _total_steps < c.warm_up_steps:
+          # track speed
+          epochs_seen = (_total_steps * bs) / train_epoch_size
+          time_done = (time.time() - _prev_time) / 60.0 # in minutes
+          step_speed = (i - _prev_step) / time_done
+          stories_speed = step_speed * bs
+          _prev_time = time.time()
+          _prev_step = i
 
-  return valid_acc, valid_cost, train_acc, train_cost 
+          pctx.profiler.add_step(i, run_meta)
+          pctx.profiler.profile_operations(options=opts)
+
+          log("{:4}: cost={:6.4f}, accuracy={:7.4f}, norm={:06.3f}, lr={:.4f} (epochs={:.1f}, steps/min={:2.0f}, stories/min={:2.0f})".format(
+                _total_steps, result[cost.name], result[accuracy.name], result[gradient_norm.name], result[_learning_rate.name],
+                epochs_seen, step_speed, stories_speed))
+
+          writer.add_summary(make_summary(tag="train/cost", value=result[cost.name]), _total_steps)
+          writer.add_summary(make_summary(tag="train/accuracy", value=result[accuracy.name]), _total_steps)
+
+        # print and log the current validation and test performance
+        if _total_steps % validate_every == 0 and _total_steps != 0:
+          log("")
+          log("task: ", c.task_id)
+          valid_cost, valid_acc = eval(prefix="valid", batch_source=valid_batch, steps=valid_steps, bs=valid_batch_size)
+          test_cost, test_acc = eval(prefix="test", batch_source=test_batch, steps=test_steps, bs=test_batch_size)
+
+          writer.add_summary(make_summary(tag="valid/cost", value=valid_cost), _total_steps)
+          writer.add_summary(make_summary(tag="valid/accuracy", value=valid_acc), _total_steps)
+          writer.add_summary(make_summary(tag="test/cost", value=test_cost), _total_steps)
+          writer.add_summary(make_summary(tag="test/accuracy", value=test_acc), _total_steps)
+
+          log("Total time passed: {:.1f} min".format((time.time() - _start_time) / 60.0))
+          log("")
+
+          if valid_acc >= _best_valid_acc:
+            saver.save(sess, os.path.join(c.log_folder, "model.ckpt"))
+
+        _total_steps += 1
+
+  return valid_acc, valid_cost, train_acc, train_cost
 
 def eval(steps=1, prefix="", batch_source=valid_batch, bs=valid_epoch_size):
   acc_sum, cost_sum = 0.0, 0.0
@@ -397,7 +431,7 @@ def eval(steps=1, prefix="", batch_source=valid_batch, bs=valid_epoch_size):
     }
     result = sess.run(query_dic, feed_dic)
     cost_sum += result[cost.name]
-    acc_sum += result[accuracy.name]    
+    acc_sum += result[accuracy.name]
 
   # the following fetch is only to trigger caching and have the cache warning go away
   try:
@@ -426,11 +460,11 @@ def translate(nparr, id2word=id2word):
   as_string = np.apply_along_axis(lambda x: " ".join(x), axis=-1, arr=arr)
   return as_string
 
-def show_random_sample():  
+def show_random_sample():
   idx = np.random.randint(2000)
-  batch = [raw_test[0][np.newaxis,idx,:,:], 
-           raw_test[1][np.newaxis,idx], 
-           raw_test[2][np.newaxis,idx,:], 
+  batch = [raw_test[0][np.newaxis,idx,:,:],
+           raw_test[1][np.newaxis,idx],
+           raw_test[2][np.newaxis,idx,:],
            raw_test[3][np.newaxis,idx]]
   feed_dic = get_feed_dic(batch)
 
@@ -485,7 +519,7 @@ def make_eval_tensors():
   eval_valid_test = []
   for i in range(1,21):
     _, raw_valid, raw_test, _ = transform_task(i)
-    valid_epoch_size = raw_valid[0].shape[0]   
+    valid_epoch_size = raw_valid[0].shape[0]
     test_epoch_size = raw_test[0].shape[0]
 
     valid_data = tf.data.Dataset.from_tensor_slices((raw_valid[0],raw_valid[1],raw_valid[2],raw_valid[3])).batch(valid_epoch_size)
